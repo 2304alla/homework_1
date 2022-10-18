24d20dfff02f55c116ebcad016726f7ba6eaa7e8
allakoval <2448572@gmail.com>
2022-10-18 21:45:46 +0300

    task5.1

---
+++ task8/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 8: Напишите программу, 
+//которая на вход принимает число (N), 
+//а на выходе показывает все чётные числа от 1 до N.
+
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+
+Console.Write("Ведите число:  ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+
+if (num < 1)
+{
+    Console.Write("Не верное значение");
+}
+else
+{
+    int count = -num;
+    while (count <= num)
+    
+    {
+        if (count % 2 == 0)
+        {
+        Console.Write($"{count}"  );
+        }
+        
+     else count++;
+    }
+
+
+
+
+
